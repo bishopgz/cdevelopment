ae800f3db3259195b8a7d2f6da0f013377015cbf
George Zafiris <zaf.mitx@gmail.com>
2019-03-14 21:02:35 +0200

    Make binary file and write struct

--- structs-and-padding/main.cpp
+++ structs-and-padding/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 /*
@@ -25,7 +26,39 @@ int main(){
 		happens to the general size 
 		of the struct and with it. 
 	*/
+	/* cout << sizeof(Generic); */
+	const char * fileName = "test.bin";
+	ofstream outFile;
+	outFile.open(fileName, ios::binary);
 	
-	cout << sizeof(Generic);
+	Generic object = {"generic", 23000, 198.345};
+	
+	if (outFile.is_open()){
+		/* Write the struct to the file */
+		// outFile.write((char *)&object, sizeof(Generic)); // old way
+		outFile.write(reinterpret_cast<char *>(&object), sizeof(Generic));
+		outFile.close();
+		cout << "Created file {" << fileName << "} successfully." << endl;
+	} else {
+		cout << "Couldn't create file {" << fileName << "}" << endl;
+	}
+	
+	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
